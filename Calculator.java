323d4147059ebf44c787ad373eac11822b4edd50
chaitanya Sonawane <sonawanechaitanya19@gmail.com>
2026-03-09 11:16:28 +0000

    initial commit

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + sub);
+        System.out.println("Multiplication: " + mul);
+        System.out.println("Division: " + div);
+    }
+}
